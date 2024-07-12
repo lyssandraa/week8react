918fb39a885c9f5028427b688d93edc7d5872d9a
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-07-12 11:38:47 +0100

    feature: starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 
+import Layout from "./components/layout/Layout";
+import MainContainer from "./components/mainContainer/MainContainer";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Layout>
+        <MainContainer />
+      </Layout>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Cute Books</h2>
+      <p>Message here</p>
+    </header>
+  );
+};
+
+export default Header;
