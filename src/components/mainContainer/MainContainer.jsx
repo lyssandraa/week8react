e72ecaa982481edd7c65468b9da8c3ef725a7897
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-07-12 14:20:25 +0100

    feature:  AddAuthor component

--- src/components/mainContainer/MainContainer.jsx
+++ src/components/mainContainer/MainContainer.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import "./MainContainer.css";
 
 import MessageContainer from "../messageContainer/MessageContainer";
+import AddAuhtor from "../addAuthor/AddAuthor";
 
 const MainContainer = () => {
   return (
     <div className="wrapper">
       <MessageContainer />
+      <AddAuhtor />
     </div>
   );
 };
